c47c8a07da8c11ea681b0e2d51d9b446aba80165
Kenneth <liekennie@gmail.com>
2026-05-07 10:10:47 +0800

    Initial Commit

---
+++ FitnessCoachApp/FitnessCoachAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessCoachAppApp.swift
+//  FitnessCoachApp
+//
+//  Created by 30 on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct FitnessCoachAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
